fc3a0b69c660cecce3c37ca634dcf24abad03966
tugbaa <tugbacevizci.840@gmail.com>
2016-12-29 01:33:45 +0300

    fixed static file

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
